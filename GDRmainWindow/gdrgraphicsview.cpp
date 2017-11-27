b5cf86fd444661b98a1c7986f67782a0798a8149
Rational-pi <1salmieri.nicolas@gmail.com>
2017-11-27 11:42:50 +0100

    GDR graphicsView proper background

--- GDRmainWindow/gdrgraphicsview.cpp
+++ GDRmainWindow/gdrgraphicsview.cpp
@@ -1,12 +1,41 @@
 #include "gdrgraphicsview.h"
 #include <QtWidgets>
+#include <QRect>
 
 #define CTRL Qt::ControlModifier & QApplication::keyboardModifiers()
+GDRgraphicsView::GDRgraphicsView(QWidget *parent):QGraphicsView(parent),m_scale(1){
+    bkgBrush=QBrush(QPixmap("://GDRres/BKGtexture.png"));
+    bkgBrush.setStyle(Qt::TexturePattern);
+    borderPen.setStyle(Qt::SolidLine);
+    borderPen.setColor(QColor(Qt::gray));
+}
+
 void GDRgraphicsView::wheelEvent(QWheelEvent *event){
     bool up = event->delta()>0;
     if (CTRL){
         setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
         qreal scaleFactor=up?1.1:0.9;
         scale(scaleFactor,scaleFactor);
+        m_scale*=scaleFactor;
     }else QGraphicsView::wheelEvent(event);
+}
+
+void GDRgraphicsView::drawBackground(QPainter *painter, const QRectF &rect){
+    painter->scale(1/m_scale, 1/m_scale);
+        // background color
+        painter->fillRect(-1+rect.x()*m_scale,-1+rect.y()*m_scale,1+geometry().width(),1+geometry().height(),Qt::lightGray);
+
+        // graphics background and border
+        painter->setBrush(bkgBrush);
+        painter->setPen(borderPen);
+        QRectF sR = this->scene()->sceneRect();
+        painter->drawRect(sR.x()*m_scale,sR.y()*m_scale,sR.width()*m_scale,sR.height()*m_scale);
+
+        painter->scale(m_scale, m_scale);
+}
+
+void GDRgraphicsView::mousePressEvent(QMouseEvent *event){
+    QPointF pt = mapToScene(event->pos());
+    float rad=0.1;
+    scene()->addLine(pt.x()-0.25,pt.y(),pt.x()+0.25,pt.y());
 }
--- GDRmainWindow/gdrgraphicsview.h
+++ GDRmainWindow/gdrgraphicsview.h
@@ -5,10 +5,16 @@
 class GDRgraphicsView : public QGraphicsView
 {
 public:
-    GDRgraphicsView(QWidget *parent = Q_NULLPTR):QGraphicsView(parent){}
+    GDRgraphicsView(QWidget *parent = Q_NULLPTR);
 
 protected:
     virtual void wheelEvent(QWheelEvent *event);
+    virtual void drawBackground(QPainter *painter, const QRectF &rect);
+    virtual void mousePressEvent(QMouseEvent *event);
+private:
+    qreal m_scale;
+    QBrush bkgBrush;
+    QPen borderPen;
 };
 
 #endif // GDRGRAPHICSVIEW_H
